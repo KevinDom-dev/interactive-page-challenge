518e50c3f3615d66d4b594865a50e157e3a9ea01
cheungkinwong <cheungkin.wong@gmail.com>
2019-08-01 11:09:14 +0200

    add showslide, next and previous slide function

---
+++ Assets/JS/main.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+ShowSlide(slideIndex);
+
+//go to next slide
+function NextSlide() {
+     ShowSlide((slideIndex += 1));
+}
+
+//go to previous slide
+function PreviousSlide() {
+     ShowSlide((slideIndex -= 1));
+}
+
+//show the slide with the number of slideIndex
+function ShowSlide(slideIndex) {
+     const slides = document.getElementsByClassName("prideslides");
+     let currentslide = slideIndex;
+
+     //return to first slide
+     if (currentslide > slides.length) {
+          slideIndex = 1;
+     }
+
+     //return to last slide
+     if (currentslide < 1) {
+          slideIndex = slides.length;
+     }
+
+     //turn off all slides
+     for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+     }
+
+     //display current slide
+     slides[slideIndex].style.display = "block";
+}
